2dddfdbc4566bbf3cd0b642f82722fd2f87b4664
jnj0405 <jnj0405@gmail.com>
2020-04-01 12:53:29 +0900

    no message

--- src/test/java/net/ecbank/fwk/admin/EcEGovAdminApplicationTests.java
+++ src/test/java/net/ecbank/fwk/admin/EcEGovAdminApplicationTests.java
@@ -1,5 +1,11 @@
 package net.ecbank.fwk.admin;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import javax.management.JMX;
 import javax.management.MBeanServerConnection;
 import javax.management.ObjectName;
@@ -7,8 +13,12 @@ import javax.management.remote.JMXConnector;
 import javax.management.remote.JMXConnectorFactory;
 import javax.management.remote.JMXServiceURL;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.CollectionUtils;
 
 import net.ecbank.fwk.admin.jmx.ServerConfigMBean;
 
@@ -16,7 +26,7 @@ import net.ecbank.fwk.admin.jmx.ServerConfigMBean;
 @SpringBootTest
 class EcEGovAdminApplicationTests {
 
-	@Test
+	//@Test
 	void contextLoads() throws Exception {
 		
 		JMXServiceURL jmxServiceURL = new JMXServiceURL("service:jmx:rmi:///jndi/rmi://localhost:9875/jmxrmi"); //,service:jmx:rmi:///jndi/rmi://localhost:9875/server,"service:jmx:rmi://localhost:9875"
@@ -25,6 +35,41 @@ class EcEGovAdminApplicationTests {
         ObjectName mbeanName = new ObjectName("ec-eGov:name=ServerConfigManageService");
         ServerConfigMBean mBeanProxy = JMX.newMBeanProxy(mbsc, mbeanName, ServerConfigMBean.class, true);
         mBeanProxy.reloadRolesAndUrlMapping();
+	}
+	
+	/**
+	 * 전자정부 테이블 중 필요없는 테이블 삭제 스크립트 생성.
+	 * 단, COMTECOPSEQ 는 지우지 말 것.
+	 * @throws IOException
+	 */
+	@Test
+	void findTableForDelete() throws IOException {
+		List<String> deleteTables = new ArrayList<String>();
+		
+		File tableFile = new File("D:\\Dev\\eGovFrameDev-3.9.0-64bit\\workspace\\ec-eGov-admin\\script\\EGOV_ALL_TABLE_LIST.txt");
+		List<String> readLines = FileUtils.readLines(tableFile, "UTF-8");
+		
+		for (String tableName : readLines) {
+			boolean isExist = false;
+			//System.out.println(tableName);
+			Iterator<File> queryFile = FileUtils.iterateFiles(new File("D:\\Dev\\eGovFrameDev-3.9.0-64bit\\workspace\\ec-eGov\\src\\main\\resources\\egovframework\\mapper\\com"), new String[]{"xml"}, true);
+			
+			while (queryFile.hasNext() && !isExist) {
+				File file = (File) queryFile.next();
+				String query = FileUtils.readFileToString(file, "UTF-8");
+				if (StringUtils.containsIgnoreCase(query, tableName)) {
+					isExist = true;
+					//System.out.println(tableName);
+				}
+			}
+			if (!isExist && !ArrayUtils.contains(ArrayUtils.toArray(deleteTables),tableName)) {
+				deleteTables.add(tableName);
+			}
+		}
+		System.out.println("총:"+deleteTables.size()+"개");
+		for (String string : deleteTables) {
+			System.out.println("DROP TABLE "+string+" CASCADE CONSTRAINTS;");
+		}
 	}
 
 }
