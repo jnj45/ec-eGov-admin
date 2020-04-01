4e96d7caee696b9c759d24936804e961d291d40c
bio1215 <bio1215@DESKTOP-PFRQ99P>
2020-04-01 11:53:55 +0900

    -

--- src/main/java/net/ecbank/fwk/admin/config/WebConfig.java
+++ src/main/java/net/ecbank/fwk/admin/config/WebConfig.java
@@ -2,9 +2,11 @@ package net.ecbank.fwk.admin.config;
 
 import java.time.Duration;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebConfig implements WebMvcConfigurer{
 	
 	/**
